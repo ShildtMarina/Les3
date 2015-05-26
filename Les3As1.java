211498400766a997d90f23f12544350c564b1f40
ShildtMarina <savchenko.lsc@gmail.com>
2015-05-26 20:06:04 +0300

    Create Les3As1.java

---
+++ Les3As1.java
@@ -0,0 +1,31 @@
+package les3as1;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Marina
+ */
+public class Les3As1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num;
+        int max = 0;
+
+        Scanner cs = new Scanner(System.in);
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Input number:");
+            num = cs.nextInt();
+            if (max <= num) {
+                max = num;
+            }
+        }
+        System.out.println(max);
+        cs.close();
+    }
+
+}
